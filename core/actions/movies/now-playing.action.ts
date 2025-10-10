5a156d32c5d522b34ef5500aaf7228f410161151
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2025-10-10 10:03:18 +0200

    Implement movie mapping and update now playing action to return mapped results

--- core/actions/movies/now-playing.action.ts
+++ core/actions/movies/now-playing.action.ts
@@ -1,11 +1,12 @@
 import { movieApi } from '@/core/api/movie-api';
 import { MovieDBMoviesResponse } from '@/core/infrastructure/interfaces/moviedb-response';
+import { MovieMapper } from '@/core/infrastructure/mappers/movie.mapper';
 
 export const nowPlayingAction = async () => {
   try {
     const { data } = await movieApi.get<MovieDBMoviesResponse>('/now_playing');
 
-    return data;
+    return data.results.map(MovieMapper.fromTheMovieDBToMovie);
   } catch (error) {
     console.log(error);
     throw 'Cannot load now playing movies';
