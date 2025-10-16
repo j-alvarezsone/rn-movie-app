6930b512c34e3ebe81cac1e599d3a5f827993929
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2025-10-16 09:56:38 +0200

    feat: add expo-linear-gradient for enhanced MovieHeader background

--- presentation/components/movie/MovieHeader.tsx
+++ presentation/components/movie/MovieHeader.tsx
@@ -1,4 +1,5 @@
 import { Ionicons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
 import { router } from 'expo-router';
 import { Image, Pressable, Text, useWindowDimensions, View } from 'react-native';
 
@@ -12,6 +13,11 @@ const MovieHeader = ({ poster, originalTitle, title }: Props) => {
   const { height: screenHeight } = useWindowDimensions();
   return (
     <>
+      <LinearGradient
+        style={{ height: screenHeight * 0.4, position: 'absolute', zIndex: 1, width: '100%' }}
+        start={[0, 0]}
+        colors={['rgba(0,0,0,0.3)', 'transparent']}
+      />
       <View className='absolute z-50 top-14 left-3 elevation-lg'>
         <Pressable onPress={() => router.dismiss()}>
           <Ionicons name='arrow-back' size={30} color='white' className='shadow' />
